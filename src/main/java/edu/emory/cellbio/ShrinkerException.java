c6310aa17eb41b785d775e16c856a76cda59e84a
Benjamin Nanes <bnanes@emory.edu>
2013-10-09 09:59:48 -0400

    Create ShrinkerException

---
+++ src/main/java/edu/emory/cellbio/ShrinkerException.java
@@ -0,0 +1,24 @@
+package edu.emory.cellbio;
+
+/**
+ *
+ * @author Benjamin Nanes
+ */
+public class ShrinkerException extends Exception {
+
+    public ShrinkerException() {
+    }
+
+    public ShrinkerException(String msg) {
+        super(msg);
+    }
+
+    public ShrinkerException(Throwable cause) {
+        super(cause);
+    }
+
+    public ShrinkerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
